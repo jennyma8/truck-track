9c70a8e10e4ffe447eb17fe0301858b150ee2935
Jenny Ma <jenny_lau_8@hotmail.com>
2024-05-05 16:01:06 -0400

    build working, nodule module rm

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Form from './Form';
 
-const Stack = createStackNavigator();
+
 
 export default function App() {
   
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Form">
-        <Stack.Screen name="Form" component={Form} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+    <Form />
+  </View>
   );
 }
 
